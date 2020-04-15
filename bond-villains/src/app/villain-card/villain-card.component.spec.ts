fd290f4dd84b2a9fe3ea7fc00dcb1fb52dd3b249
Björn Weström <bjowes@gmail.com>
2020-04-15 23:08:52 +0200

    package bump fixed jasmine tests

--- bond-villains/src/app/villain-card/villain-card.component.spec.ts
+++ bond-villains/src/app/villain-card/villain-card.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VillainCardComponent } from './villain-card.component';
+import { VillainService } from '../villain.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('VillainCardComponent', () => {
   let component: VillainCardComponent;
   let fixture: ComponentFixture<VillainCardComponent>;
+  let villainServiceMock: jasmine.SpyObj<VillainService>;
 
   beforeEach(async(() => {
+    villainServiceMock = jasmine.createSpyObj<VillainService>(
+      'VillainService',
+      ['randomVillains', 'cachedVillains']
+    );
+
     TestBed.configureTestingModule({
-      declarations: [ VillainCardComponent ]
-    })
-    .compileComponents();
+      declarations: [VillainCardComponent],
+      providers: [{ provide: VillainService, useValue: villainServiceMock }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
